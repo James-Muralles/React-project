d093c011d46fba898814ee58b2da28db1203039b
James Muralles <james.muralles23@gmail.com>
2020-06-19 23:36:01 -0500

    adding input functinallity

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -14,13 +14,45 @@ class SearchBar extends React.Component {
             term: '',
             location: '',
             sortBy: 'best_match'
+
         };
+
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
     }
+
+    getSortByClass(sortByOption) {
+        if (sortByOption === this.state.sortBy) {
+            return 'active';
+        } else {
+            return '';
+        }
+    }
+
+    handleSortByChange(sortByOption) {
+        this.setState({sortBy: sortByOption})
+    }
+
+    handleTermChange(e) {
+        this.setState({term: e.target.value})
+    }
+
+    handleLocationChange(e) {
+        this.setState({location: e.target.value})
+    }
+    handleSearch(e){
+        this.props.searchYelp(
+
+        )
+
+    }
+
 
     renderSortByOptions() {
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>
+            return <li onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+                       className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption}</li>
         });
     }
 
@@ -32,8 +64,8 @@ class SearchBar extends React.Component {
                 </ul>
             </div>
             <div className="SearchBar-fields">
-                <input placeholder="Search Businesses"/>
-                <input placeholder="Where?"/>
+                <input onChange={this.handleTermChange} placeholder="Search Businesses"/>
+                <input onChange={this.handleLocationChange} placeholder="Where?"/>
             </div>
             <div className="SearchBar-submit">
                 <a>Let's Go</a>
